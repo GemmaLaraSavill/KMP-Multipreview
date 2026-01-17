cb333e83e2e3072d88d06d15ab196b7cba693c23
GLS <gemmalarasav@gmail.com>
2026-01-17 18:46:23 +0100

    Remove boilerplate and unused code

--- composeApp/src/commonMain/kotlin/dev/myhappyplace/multipreviews/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.myhappyplace.multipreviews
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/dev/myhappyplace/multipreviews/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.myhappyplace.multipreviews
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
